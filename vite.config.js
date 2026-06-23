a4c279f167c0062a054a39d05a5ac37b8e4ae4de
Daniel Marino <iamdanielmarino@gmail.com>
2026-06-23 09:56:06 -0400

    organize files a bit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { ViteEjsPlugin } from 'vite-plugin-ejs'
 
 export default defineConfig({
+  root: 'src',
   server: { hmr: false },
   plugins: [
     ViteEjsPlugin({
